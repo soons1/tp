aab10b79ae44f1ddaf02cda9c9f3ace659650b5b
Ger Teck <111064611+gerteck@users.noreply.github.com>
2024-03-28 18:42:21 +0800

    Add testcases for EditCommand

--- src/test/java/scrolls/elder/logic/commands/EditCommandTest.java
+++ src/test/java/scrolls/elder/logic/commands/EditCommandTest.java
@@ -66,7 +66,7 @@ public class EditCommandTest {
 
 
     @Test
-    public void execute_someFieldsSpecifiedUnfilteredList_success() {
+    public void execute_someFieldsSpecifiedUnfilteredVolunteerList_success() {
 
         Index indexSecondPerson = Index.fromOneBased(2);
         Person secondPerson = personStore.getFilteredVolunteerList().get(indexSecondPerson.getZeroBased());
@@ -101,6 +101,13 @@ public class EditCommandTest {
         String expectedMessage = String.format(EditCommand.MESSAGE_EDIT_PERSON_SUCCESS, Messages.format(editedPerson));
 
         CommandTestUtil.assertCommandSuccess(editCommand, model, expectedMessage, expectedModel);
+    }
+
+    @Test
+    public void execute_noRoleNoFieldsSpecifiedUnfilteredVolunteerList_failure() {
+        EditCommand.EditPersonDescriptor epd = new EditCommand.EditPersonDescriptor();
+        EditCommand editCommand = new EditCommand(TypicalIndexes.INDEX_FIRST_PERSON, epd);
+        CommandTestUtil.assertCommandFailure(editCommand, model, EditCommand.MESSAGE_NO_ROLE);
     }
 
     @Test
