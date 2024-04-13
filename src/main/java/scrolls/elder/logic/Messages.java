cd647f5cd3ab42935c4a6b3a6e59aa44ffc75a54
sdevih <e0960282@u.nus.edu>
2024-04-14 01:32:52 +0800

    Fix punctuation for error message
    Full stops were added to error messages for pair command
    which are deemed fixable bugs.

--- src/main/java/scrolls/elder/logic/Messages.java
+++ src/main/java/scrolls/elder/logic/Messages.java
@@ -16,8 +16,8 @@ public class Messages {
 
     public static final String MESSAGE_UNKNOWN_COMMAND = "Unknown command";
     public static final String MESSAGE_INVALID_COMMAND_FORMAT = "Invalid command format! \n%1$s";
-    public static final String MESSAGE_INVALID_PERSON_DISPLAYED_INDEX = "The person index provided is invalid";
-    public static final String MESSAGE_INVALID_LOG_DISPLAYED_INDEX = "The log index provided is invalid";
+    public static final String MESSAGE_INVALID_PERSON_DISPLAYED_INDEX = "The person index provided is invalid.";
+    public static final String MESSAGE_INVALID_LOG_DISPLAYED_INDEX = "The log index provided is invalid.";
     public static final String MESSAGE_PERSONS_LISTED_OVERVIEW = "%1$d persons listed!";
     public static final String MESSAGE_PERSONS_LISTED_OVERVIEW_WITH_ROLE = "%1$d persons listed with role %2$s!";
 
